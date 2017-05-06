db8c49d7ff57d8b7a927f9306f59d2062c826856
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-05-06 22:19:27 +0200

    Adding error message

--- src/app/Components/ActionsBar.java
+++ src/app/Components/ActionsBar.java
@@ -1,13 +1,18 @@
 package app.Components;
 
+import app.Navigator;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class ActionsBar {
 
     private static ActionsBar instance;
-    private GridPane actionBarLayout;
+    private BorderPane actionBarLayout;
+    private GridPane buttonsLayout;
+    private Label errorMessage;
     private Button scanButton;
     private Button parseButton;
     private Button compileButton;
@@ -17,9 +22,15 @@ public class ActionsBar {
     }
 
     private void render() {
+        //Error Message
+        errorMessage = new Label("1 error, at line 2");
+        errorMessage.getStyleClass().add("error-message");
+
         //Buttons
         scanButton = new Button("Scan");
         scanButton.getStyleClass().add("btn");
+
+        scanButton.setOnAction(e -> Navigator.viewPage());
 
         parseButton = new Button("Parse");
         parseButton.getStyleClass().add("btn");
@@ -27,17 +38,26 @@ public class ActionsBar {
         compileButton = new Button("Compile");
         compileButton.getStyleClass().add("btn");
 
-        //Action bar container
-        actionBarLayout = new GridPane();
-        actionBarLayout.setAlignment(Pos.CENTER_RIGHT);
-        actionBarLayout.getStyleClass().add("action-bar");
+        //Buttons layout
+        buttonsLayout = new GridPane();
+        buttonsLayout.setAlignment(Pos.CENTER_RIGHT);
         GridPane.setConstraints(scanButton, 0, 0);
         GridPane.setConstraints(parseButton, 1, 0);
         GridPane.setConstraints(compileButton, 2, 0);
-        actionBarLayout.getChildren().addAll(scanButton, parseButton, compileButton);
+        buttonsLayout.getChildren().addAll(scanButton, parseButton, compileButton);
+
+        //Action bar layout
+        actionBarLayout = new BorderPane();
+        actionBarLayout.getStyleClass().add("action-bar");
+        actionBarLayout.setRight(buttonsLayout);
+        actionBarLayout.setLeft(errorMessage);
     }
 
-    public GridPane getActionBar() {
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage.setText(errorMessage);
+    }
+
+    public BorderPane getActionBar() {
         return actionBarLayout;
     }
 
