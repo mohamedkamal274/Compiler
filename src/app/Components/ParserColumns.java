500dab3038d2a6a3a4b99ee4d8df462740299611
mohamedkamal274 <muhammedkmal3@gmail.com>
2017-05-10 14:48:08 +0200

    Final Beta discussion

--- src/app/Components/ParserColumns.java
+++ src/app/Components/ParserColumns.java
@@ -20,22 +20,17 @@ public class ParserColumns {
         parserColumns = new ArrayList<>();
 
         TableColumn<Output, String> index = new TableColumn("No.");
-        index.setMaxWidth(1f * Integer.MAX_VALUE * 7);
+        index.setMaxWidth(1f * Integer.MAX_VALUE * 10);
         index.setCellValueFactory(new PropertyValueFactory<>("index"));
         parserColumns.add(index);
 
         TableColumn<Output, String> rule = new TableColumn("Rule");
-        rule.setMaxWidth(1f * Integer.MAX_VALUE * 31);
+        rule.setMaxWidth(1f * Integer.MAX_VALUE * 45);
         rule.setCellValueFactory(new PropertyValueFactory<>("rule"));
         parserColumns.add(rule);
-
-        TableColumn<Output, String> input = new TableColumn("Input");
-        input.setMaxWidth(1f * Integer.MAX_VALUE * 31);
-        input.setCellValueFactory(new PropertyValueFactory<>("input"));
-        parserColumns.add(input);
-
+        
         TableColumn<Output, String> process = new TableColumn("Process");
-        process.setMaxWidth(1f * Integer.MAX_VALUE * 31);
+        process.setMaxWidth(1f * Integer.MAX_VALUE * 45);
         process.setCellValueFactory(new PropertyValueFactory<>("process"));
         parserColumns.add(process);
     }
